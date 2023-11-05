fc31ddb8430df58806fdf039445ce470afbda378
Aditya Naresh <adityanaresh007@gmail.com>
2023-11-05 21:46:43 +0530

    Invoice downloadable

--- ecommerce/orders/views.py
+++ ecommerce/orders/views.py
@@ -1,14 +1,65 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse  
 from carts.models import CartItem
 from .forms import OrderForm
 from .models import *
 import datetime
 import json
 
+from reportlab.pdfgen import canvas
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
-# Create your views here.
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from io import BytesIO
+
+def render_to_pdf(template_path, context):
+    template = get_template(template_path)
+    html = template.render(context)
+    response = BytesIO()
+    
+    pdf = pisa.pisaDocument(
+        BytesIO(html.encode("UTF-8")),
+        response,
+        pagesize='letter'  # Set the page size to letter (8.5 x 11 inches)
+    )
+    
+    if not pdf.err:
+        response.seek(0)
+        return response
+    return None
+
+def download_invoice(request, order_id):
+    order = Order.objects.get(id=order_id)
+    ordered_products = OrderProduct.objects.filter(order_id=order.id)
+    payment = Payment.objects.get(payment_id=order.payment.payment_id)
+    subtotal = 0
+
+    for item in ordered_products:
+        subtotal += item.product_price * item.quantity
+
+
+    context = {
+        'order': order,
+        'order_number': order.order_number,
+        'transID': payment.payment_id,
+        'ordered_products': ordered_products,
+        'subtotal': subtotal,
+    }
+
+    template_path = 'orders/invoice.html'  # Replace with the path to your HTML template
+    pdf_response = render_to_pdf(template_path, context)
+
+    if pdf_response:
+        response = HttpResponse(pdf_response.read(), content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename=invoice_{order.order_number}.pdf'
+        return response
+
+    # Handle the case where PDF generation fails
+    return HttpResponse('PDF generation failed', status=500)
+
+
 
 
 def payments(request):
@@ -169,6 +220,8 @@ def order_complete(request):
             'payment' : payment,
             'subtotal' :subtotal,
         }
+
+
 
     except (Payment.DoesNotExist, Order.DoesNotExist):
         return redirect('home')
